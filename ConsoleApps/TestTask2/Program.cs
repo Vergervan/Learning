d4449bc4fed1e44faea454dfabf5ab978c8bd1e9
Vergervan <krazy.vovan@mail.ru>
2021-12-01 21:06:24 +0300

    Complete task

--- ConsoleApps/TestTask2/Program.cs
+++ ConsoleApps/TestTask2/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TestTask2
 {
     public class StringIndex
     {
+        public enum StringLanguage
+        {
+            Russian, English
+        }
         public static List<char> FilterChars { get; } = new List<char>() { ' ', ',', '.', '_', '!', '?', '-', '\'', '\"' };
         private string _str;
         private string _fstr;
         private string _comment;
         private float _index;
+        private StringLanguage _lang;
         public StringIndex() { }
         public StringIndex(string srcStr)
         {
@@ -18,6 +24,7 @@ namespace TestTask2
         }
         public float Index { get => _index; }
         public string Comment { get => _comment; }
+        public StringLanguage Language { get => _lang; }
         public string FilteredString { get => _fstr; }
         public string SourceString
         {
@@ -27,6 +34,7 @@ namespace TestTask2
                 string[] parts = value.Split('|');
                 _str = parts[0];
                 if(parts.Length > 1) _comment = parts[1];
+                _lang = Regex.IsMatch(_str, @"\p{IsCyrillic}") ? StringLanguage.Russian : StringLanguage.English;
                 _fstr = FilterString(_str);
                 _index = CalculateIndex(_fstr);
             }
@@ -45,35 +53,49 @@ namespace TestTask2
         }
         private string FilterString(string str)
         {
-            FilterChars.ForEach(x => str = str.Replace(x.ToString(), String.Empty));
+            FilterChars.ForEach(x => str = str.Replace(x.ToString(), string.Empty));
             return str;
         }
     }
     class Program
     {
-        public static List<StringIndex> russianStrings = new List<StringIndex>();
-        public static List<StringIndex> englishStrings = new List<StringIndex>();
+        public static List<StringIndex> indexes = new List<StringIndex>();
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("Выберите язык строки:\n1.Русский\n2.Английский");
-                ConsoleKey key = Console.ReadKey().Key;
-                Console.Clear();
                 Console.Write("Введите строку: ");
                 string entryString = Console.ReadLine();
                 StringIndex index = new StringIndex(entryString);
-                Console.WriteLine(index.Index);
-                switch (key)
+                indexes.Add(index);
+                Console.WriteLine($"Индекс текущей строки равен {index.Index}");
+                StringIndex[] matches = FindSameIndexes(index, indexes);
+                Console.WriteLine($"Найдено {(matches == null ? 0 : matches.Length)} совпадений по индексам");
+                if(matches != null)
                 {
-                    case ConsoleKey.D1:
-                        russianStrings.Add(index);
-                        break;
-                    case ConsoleKey.D2:
-                        englishStrings.Add(index);
-                        break;
+                    for(int i = 0; i < matches.Length; i++)
+                    {
+                        Console.WriteLine($"{i + 1}.{matches[i].SourceString}");
+                    }
                 }
+                Console.WriteLine("Хотите продолжить? (y/n)");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    Console.Clear();
+                    continue;
+                }
+                return;
             }
+        }
+        static StringIndex[] FindSameIndexes(StringIndex index, IEnumerable<StringIndex> indexes)
+        {
+            List<StringIndex> matches = new List<StringIndex>();
+            foreach(var item in indexes)
+            {
+                if (item == index || index.Language == item.Language || index.Index != item.Index) continue;
+                matches.Add(item);
+            }
+            return matches.Count > 0 ? matches.ToArray() : null;
         }
     }
 }
